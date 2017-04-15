72523841224fe686c4e6a47a380e71766fc58985
Gostaug <pierrot_du34@hotmail.fr>
2017-04-15 17:41:45 +0200

    Add files via upload

--- Game.cpp
+++ Game.cpp
@@ -1,8 +1,9 @@
 #include <stdlib.h> // Clear console
-#include <fstream> // Dossiers
 #include <stdlib.h> // Random
 #include <string> // Concaténation des strings
 #include <sstream> // Nécessaire pour le patch
+#include <iostream> // I/O
+#include <Windows.h> // Pour la fonction Sleep
 
 #include "Game.h"
 
@@ -21,7 +22,7 @@ namespace patch
 
 // Constructeur & Destructeur
 Game::Game()
-	:m_nombreDePions(NBRE_PIONS), m_tourDeJouer(NOIR), m_pConsole(Console::getInstance())
+	:m_pConsole(Console::getInstance())
 {
 	system("cls");
 	system("color 80");
@@ -35,7 +36,6 @@ Game::~Game()
 	// On libère la mémoire
 	m_pConsole->deleteInstance();
 }
-
 
 // Methodes
 void Game::lancerJeux()
@@ -69,8 +69,8 @@ void Game::menu()
 
 	switch(choix)
 	{
-		case '1' : this->reinitialiserTablier(); break;
-		case '2' : this->chargerTablier(); break;
+		case '1' : m_tablier.reinitialiserTablier(); break;
+		case '2' : m_tablier.chargerTablier(); break;
 	}
 
 	choix = 0;
@@ -107,22 +107,20 @@ void Game::menu()
 	}
 }
 
-
-
 void Game::modeJoueurVJoueur()
 {
 	/// Traitement
-	while(!(this->finDuJeux())) // Tant que le joueur peut jouer un coup valide
+	while(!(m_tablier.finDuJeux())) // Tant que le joueur peut jouer un coup valide
 	{
 		this->afficherTablier(); // Affichage du tablier
 
-		if(m_tourDeJouer == NOIR) // On affiche un message indiquant quel joueur doit jouer
+		if(m_tablier.m_tourDeJouer == NOIR) // On affiche un message indiquant quel joueur doit jouer
 			this->afficherMessage("Les noirs doivent jouer");
 		else
 			this->afficherMessage("Les blancs doivent jouer");
 
 		this->joueurJoue(); // On joue un pion
-		this->avancerTour(); // On donne aux pions opposés le droit de jouer
+		m_tablier.avancerTour(); // On donne aux pions opposés le droit de jouer
 	}
 
 	this->afficherTablier(); // Petit re-affichage pour actualiser la dernière pose de pion
@@ -132,21 +130,23 @@ void Game::modeJoueurVJoueur()
 void Game::modeIARVJoueur()
 {
 	/// Traitement
-	while(!(this->finDuJeux())) // Tant que le joueur ou l'IA peut jouer un coup valide
-	{
-		this->afficherTablier(); // Affichage du tablier
+	this->afficherTablier();
 
-		if(m_tourDeJouer == NOIR) {// Le joueur joue
+	while(!(m_tablier.finDuJeux())) // Tant que le joueur ou l'IA peut jouer un coup valide
+	{
+		if(m_tablier.m_tourDeJouer == NOIR) { // Le joueur joue
 			this->afficherMessage("Les noirs doivent jouer");
 			this->joueurJoue(); // On joue un pion
 		}
-		else
+		else if(m_tablier.m_tourDeJouer == BLANC) {
+			this->afficherMessage("L'ordinateur joue      ");
+			Sleep(1000);
 			this->IARJoue(); // L'IA joue
-
-		this->avancerTour(); // On donne aux pions opposés le droit de jouer
+		}
+		this->afficherTablier(); // Affichage du tablier
+		m_tablier.avancerTour(); // On donne aux pions opposés le droit de jouer
 	}
 
-	this->afficherTablier(); // Petit re-affichage pour actualiser la dernière pose de pion
 	this->trouverVainqueur(); // On trouve le vainqueur
 }
 
@@ -163,9 +163,9 @@ void Game::joueurJoue()
 		// On sélectionne une case à l'aide des boutons z, d, s, q, et ENTER pour valider
 		this->selectionCase(xSelect, ySelect);
 
-	} while(!(this->selectionValide(xSelect, ySelect))); // Tant que la case n'est pas un choix valide
+	} while(!(m_tablier.selectionValide(xSelect, ySelect))); // Tant que la case n'est pas un choix valide
 
-	this->poserPion(xSelect, ySelect); // On pose le pion et remplace les pions ennemis aux alentours si nécessaire
+	m_tablier.poserPion(xSelect, ySelect); // On pose le pion et remplace les pions ennemis aux alentours si nécessaire
 }
 
 void Game::IARJoue()
@@ -179,12 +179,12 @@ void Game::IARJoue()
 	{
 		for(int x=0; x<TAB_TAILLE; x++)
 		{
-			if(this->selectionValide(x, y))
+			if(m_tablier.selectionValide(x, y))
 				nombreDeCoupsPossibles++;
 		}
 	}
 		// Sélection du numéro du coup choisit
-		numeroPionSelectionne = rand() % (nombreDeCoupsPossibles + 1);
+		numeroPionSelectionne = (rand() % nombreDeCoupsPossibles) +1;
 		nombreDeCoupsPossibles = 0;
 
 		// On joue le coup random
@@ -192,89 +192,16 @@ void Game::IARJoue()
 	{
 		for(int x=0; x<TAB_TAILLE; x++)
 		{
-			if(this->selectionValide(x, y)) {
+			if(m_tablier.selectionValide(x, y)) {
 				nombreDeCoupsPossibles++;
 
 				if(nombreDeCoupsPossibles == numeroPionSelectionne) {
-					this->poserPion(x, y);
+					m_tablier.poserPion(x, y);
 					return;
 				}
 			}
 		}
 	}
-
-}
-
-
-
-void Game::reinitialiserTablier()
-{
-	// Remplissage tableau
-	for(int x=0; x<TAB_TAILLE; x++)
-	{
-		for(int y=0; y<TAB_TAILLE; y++)
-		{
-			m_tablier[x][y] = VIDE;
-		}
-	}
-
-	// On pose les 4 pions initaux
-	m_tablier	[(TAB_TAILLE/2)-1]	[(TAB_TAILLE/2)-1]	= BLANC;
-	m_tablier	[TAB_TAILLE/2]		[TAB_TAILLE/2]		= BLANC;
-	m_tablier	[TAB_TAILLE/2]		[(TAB_TAILLE/2)-1]	= NOIR;
-	m_tablier	[(TAB_TAILLE/2)-1]	[TAB_TAILLE/2]		= NOIR;
-}
-
-void Game::chargerTablier()
-{
-	/// Données
-	std::string ligne; // Ligne servant à lire les données
-
-	/// Traitement
-	std::ifstream fichier(SAVEFILE, std::ios::in); // Ouverture du fichier
-
-	if(!fichier) { // Vérification
-		std::cout << "Erreur ouverture fichier: " << SAVEFILE << std::endl;
-		return;
-	}
-
-	getline(fichier, ligne); // On récupère la ligne
-	m_tourDeJouer = ligne[0]; // Cette ligne ne comportant qu'une lettre, déterminant qui joue à ce moment précis
-
-	for(int y=0; y<TAB_TAILLE; y++)
-	{
-		getline(fichier, ligne); // On récupère la ligne
-
-		for(int x=0; x<TAB_TAILLE; x++)
-		{
-			m_tablier[x][y] = ligne[x]; // On remplit le tableau
-		}
-	}
-
-	fichier.close();  // on ferme le fichier
-}
-
-void Game::sauvegarderTablier() //!\\ Changer plus tard le "text.txt" en SAVEFILE
-{
-	/// Traitement
-	std::ofstream fichier("test.txt", std::ios::out | std::ios::trunc);// Ouverture en écriture et effacage
-
-	if(!fichier) { // Vérification
-	std::cout << "Erreur ouverture fichier: " << SAVEFILE << std::endl;
-	return;
-	}
-
-	fichier << m_tourDeJouer << std::endl; // On écrit d'abord qui doit jouer à ce tour là
-
-	for(int y=0; y<TAB_TAILLE; y++)
-	{
-		for(int x=0; x<TAB_TAILLE; x++)
-		{
-			fichier << m_tablier[x][y]; // On sauvegarde le tableau
-		}
-		fichier << std::endl;
-	}
-	fichier.close();
 }
 
 
@@ -289,7 +216,7 @@ void Game::afficherTablier()
 		{
 			m_pConsole->gotoLigCol((X0 + x*ESPACEMENT_X), (Y0+ y*ESPACEMENT_Y));
 
-			switch(m_tablier[x][y])
+			switch(m_tablier.m_tab[x][y])
 			{
 			case NOIR:	m_pConsole->setColor(COLOR_BLACK, COLOR_GRAY);	std::cout << LOGO_PION; break;
 			case BLANC:	m_pConsole->setColor(COLOR_WHITE, COLOR_GRAY);	std::cout << LOGO_PION;	break;
@@ -309,7 +236,6 @@ void Game::afficherMessage(std::string message)
 
 
 
-
 void Game::selectionCase(int &x, int &y)
 {
 	/// Données
@@ -319,7 +245,7 @@ void Game::selectionCase(int &x, int &y)
 
 	// On colorie la 1ère case [0][0] pour montrer la position initiale du curseur
 	m_pConsole->gotoLigCol((X0 + x*ESPACEMENT_X), (Y0+ y*ESPACEMENT_Y));
-	switch(m_tablier[x][y])
+	switch(m_tablier.m_tab[x][y])
 	{
 	case NOIR:	m_pConsole->setColor(COLOR_BLACK, COLOR_RED);	std::cout << LOGO_PION; break;
 	case BLANC: m_pConsole->setColor(COLOR_WHITE, COLOR_RED); 	std::cout << LOGO_PION; break;
@@ -339,7 +265,7 @@ void Game::selectionCase(int &x, int &y)
 				case 'q': 	dx = -1; 	dy = 0; 	break;
 				case 'd': 	dx = 1; 	dy = 0; 	break;
 
-				case BUTTON_ESC: this->sauvegarderTablier(); // Sauvegarder la partie en cours
+				case BUTTON_ESC: m_tablier.sauvegarderTablier(); // Sauvegarder la partie en cours
 				default: 	dx = 0; 	dy = 0; 	break;
 			}
 
@@ -351,7 +277,7 @@ void Game::selectionCase(int &x, int &y)
 
 			// On regrise l'arrière plan (utiliser la fonction afficher utilise ressources) du curseur précédent
 			m_pConsole->gotoLigCol((X0 + x*ESPACEMENT_X), (Y0+ y*ESPACEMENT_Y));
-			switch(m_tablier[x][y])
+			switch(m_tablier.m_tab[x][y])
 			{
 			case NOIR:	m_pConsole->setColor(COLOR_BLACK, COLOR_GRAY);	std::cout << LOGO_PION; break;
 			case BLANC: m_pConsole->setColor(COLOR_WHITE, COLOR_GRAY); 	std::cout << LOGO_PION; break;
@@ -364,7 +290,7 @@ void Game::selectionCase(int &x, int &y)
 
 			// On colorie le curseur
 			m_pConsole->gotoLigCol((X0 + x*ESPACEMENT_X), (Y0+ y*ESPACEMENT_Y));
-			switch(m_tablier[x][y])
+			switch(m_tablier.m_tab[x][y])
 			{
 			case NOIR:	m_pConsole->setColor(COLOR_BLACK, COLOR_RED);	std::cout << LOGO_PION; break;
 			case BLANC: m_pConsole->setColor(COLOR_WHITE, COLOR_RED); 	std::cout << LOGO_PION; break;
@@ -375,107 +301,6 @@ void Game::selectionCase(int &x, int &y)
 	}while(bouton != BUTTON_ENTER); // Tant que l'on a pas appuyé sur Enter
 
 	m_pConsole->setColor(COLOR_BLACK, COLOR_GRAY);
-}
-
-bool Game::selectionValide(int x, int y)
-{
-	/// Données
-	char pionEnnemi, pion = m_tourDeJouer;
-
-	/// Traitement
-	if(pion == NOIR) 	pionEnnemi = BLANC;
-	if(pion == BLANC) 	pionEnnemi = NOIR;
-
-	if(!(this->caseLibre(x,y))) // 1er test
-		return false;
-
-	if((x-2 >= 0) && (y-2 >= 0))
-		if(	(m_tablier[x-1][y-1] == pionEnnemi) 	&& 		(m_tablier[x-2][y-2] == pion)	)	return true; // Haut-Gauche
-	if(y-2 >= 0)
-		if(	(m_tablier[x][y-1] == pionEnnemi) 		&&		(m_tablier[x][y-2] == pion)		)	return true; // Haut
-	if((x+2 < TAB_TAILLE) && (y-2 >= 0))
-		if(	(m_tablier[x+1][y-1] == pionEnnemi) 	&& 		(m_tablier[x+2][y-2] == pion) 	)	return true; // Haut-Droit
-
-	if(x-2 >= 0)
-		if(	(m_tablier[x-1][y] == pionEnnemi) 		&& 		(m_tablier[x-2][y] == pion)		)	return true; // Middle-Gauche
-	if(x+2 < TAB_TAILLE)
-		if(	(m_tablier[x+1][y] == pionEnnemi) 		&& 		(m_tablier[x+2][y] == pion)		)	return true; // Middle-Droit
-
-	if((x-2 >= 0) && (y+2 < TAB_TAILLE))
-		if(	(m_tablier[x-1][y+1] == pionEnnemi) 	&& 		(m_tablier[x-2][y+2] == pion)	)	return true; // Bas-Gauche
-	if(y+2 < TAB_TAILLE)
-		if(	(m_tablier[x][y+1] == pionEnnemi) 		&&		(m_tablier[x][y+2] == pion)		)	return true; // Bas
-	if((x+2 < TAB_TAILLE) && (y+2 < TAB_TAILLE))
-		if(	(m_tablier[x+1][y+1] == pionEnnemi) 	&& 		(m_tablier[x+2][y+2] == pion) 	)	return true; // Bas-Droit
-
-	// Si aucune des conditions précédentes n'a retourné vraie
-	return false;
-}
-
-void Game::poserPion(int x, int y)
-{
-	/// Données
-	char pionEnnemi, pion = m_tourDeJouer;
-
-	/// Traitement
-	if(pion == NOIR) 	pionEnnemi = BLANC;
-	if(pion == BLANC) 	pionEnnemi = NOIR;
-
-	/// Traitement
-	// Pion au milieu
-	if((x-2 >= 0) && (y-2 >= 0))
-		if(	(m_tablier[x-1][y-1] == pionEnnemi) 	&& 		(m_tablier[x-2][y-2] == pion)	)	m_tablier[x-1][y-1] = pion; // Haut-Gauche
-	if(y-2 >= 0)
-		if(	(m_tablier[x][y-1] == pionEnnemi) 		&&		(m_tablier[x][y-2] == pion)		)	m_tablier[x][y-1] 	= pion; // Haut
-	if((x+2 < TAB_TAILLE) && (y-2 >= 0))
-		if(	(m_tablier[x+1][y-1] == pionEnnemi) 	&& 		(m_tablier[x+2][y-2] == pion) 	)	m_tablier[x+1][y-1] = pion; // Haut-Droit
-
-	if(x-2 >= 0)
-		if(	(m_tablier[x-1][y] == pionEnnemi) 		&& 		(m_tablier[x-2][y] == pion)		)	m_tablier[x-1][y] = pion; // Middle-Gauche
-	if(x+2 < TAB_TAILLE)
-		if(	(m_tablier[x+1][y] == pionEnnemi) 		&& 		(m_tablier[x+2][y] == pion)		)	m_tablier[x+1][y] = pion; // Middle-Droit
-
-	if((x-2 >= 0) && (y+2 < TAB_TAILLE))
-		if(	(m_tablier[x-1][y+1] == pionEnnemi) 	&& 		(m_tablier[x-2][y+2] == pion)	)	m_tablier[x-1][y+1] = pion; // Bas-Gauche
-	if(y+2 < TAB_TAILLE)
-		if(	(m_tablier[x][y+1] == pionEnnemi) 		&&		(m_tablier[x][y+2] == pion)		)	m_tablier[x][y+1] 	= pion; // Bas
-	if((x+2 < TAB_TAILLE) && (y+2 < TAB_TAILLE))
-		if(	(m_tablier[x+1][y+1] == pionEnnemi) 	&& 		(m_tablier[x+2][y+2] == pion) 	)	m_tablier[x+1][y+1] = pion; // Bas-Droit
-
-	// Pion à l'endroit souhaité
-	m_tablier[x][y] = pion;
-}
-
-bool Game::caseLibre(int x, int y)
-{
-	if(m_tablier[x][y] == VIDE)
-		return true;
-	else
-		return false;
-}
-
-
-
-void Game::avancerTour()
-{
-	if(m_tourDeJouer == NOIR)
-		m_tourDeJouer = BLANC;
-	else
-		m_tourDeJouer = NOIR;
-}
-
-bool Game::finDuJeux()
-{
-	for(int y=0; y<TAB_TAILLE; y++)
-	{
-		for(int x=0; x<TAB_TAILLE; x++)
-		{
-			if(this->selectionValide(x, y))
-				return false;
-		}
-	}
-	// Si aucune condition n'a renvoyé false, donc qu'il n'y a pas de sélection valide que l'on peut faire, c'est la fin du jeux
-	return true;
 }
 
 void Game::trouverVainqueur()
@@ -489,8 +314,8 @@ void Game::trouverVainqueur()
 	{
 		for(int x=0; x<TAB_TAILLE; x++)
 		{
-			if(m_tablier[x][y] == BLANC) 	compteurBlanc++; // Incrémentation du compteur
-			if(m_tablier[x][y] == NOIR)		compteurNoir++;
+			if(m_tablier.m_tab[x][y] == BLANC) 	compteurBlanc++; // Incrémentation du compteur
+			if(m_tablier.m_tab[x][y] == NOIR)	compteurNoir++;
 		}
 	}
 
@@ -501,15 +326,4 @@ void Game::trouverVainqueur()
 	phraseAnnonce += " Score: " + patch::to_string(compteurBlanc) + "-" + patch::to_string(compteurNoir); // Score en plus
 
 	this->afficherMessage(phraseAnnonce);
-}
-
-// Getters & Setters
-int Game::getNombreDePions()
-{
-	return m_nombreDePions;
-}
-
-char * Game::getTableau()
-{
-	return &m_tablier[TAB_TAILLE][TAB_TAILLE];
 }
